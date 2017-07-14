580d592b32dd98daa0f435eb3ee812476e86a877
dindonkey <simone.caldon@gmail.com>
2017-07-14 14:19:28 +0200

    refactor

--- ChuckNorrisJokesMVVM/JokesList/JokesViewController.swift
+++ ChuckNorrisJokesMVVM/JokesList/JokesViewController.swift
@@ -6,12 +6,10 @@ class JokesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
