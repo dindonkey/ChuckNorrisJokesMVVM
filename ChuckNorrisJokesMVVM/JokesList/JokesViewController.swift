ee45d0063f99166671a6a267a4604aafc09d0745
dindonkey <simone.caldon@gmail.com>
2017-07-14 14:17:17 +0200

    tableview added

--- ChuckNorrisJokesMVVM/JokesList/JokesViewController.swift
+++ ChuckNorrisJokesMVVM/JokesList/JokesViewController.swift
@@ -1,15 +1,9 @@
-//
-//  ViewController.swift
-//  ChuckNorrisJokesMVVM
-//
-//  Created by Dindonkey on 13/07/2017.
-//  Copyright © 2017 Dindonkey. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class JokesViewController: UIViewController {
 
+    @IBOutlet weak var jokesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
