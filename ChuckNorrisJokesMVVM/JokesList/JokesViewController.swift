f2cf77ee2d2007ca7d4bdedeb3edb7282d0afd64
dindonkey <simone.caldon@gmail.com>
2017-07-14 14:38:44 +0200

    tableview rx bind

--- ChuckNorrisJokesMVVM/JokesList/JokesViewController.swift
+++ ChuckNorrisJokesMVVM/JokesList/JokesViewController.swift
@@ -1,11 +1,23 @@
 import UIKit
+import RxSwift
+import RxCocoa
 
 class JokesViewController: UIViewController {
 
     @IBOutlet weak var jokesTableView: UITableView!
     
+    let disposeBag = DisposeBag()
+    var jokes = Variable<[String]>(["one","two"])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        jokes
+            .asObservable()
+            .bind(to: jokesTableView.rx.items(cellIdentifier: "JokeCell")) { (_, joke, cell) in
+                cell.textLabel?.text = joke
+            }
+            .addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
